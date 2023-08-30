1e19da84f701af771e943646ab619984b1594315
lucayuki1635 <lucayuki1635@gmail.com>
2023-08-29 22:38:18 -0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import './styles.css'; 
 import Navbar from '../components/layout/Navbar.js';
-import Card from '../components/layout/cards.js';
+import CardDestaque from '../components/layout/CardDestaque.js';
+import Title from '../components/layout/Title.jsx';
+import CardMenor from '../components/layout/CardMenor.jsx';
 
 export default function Home() {
+  const destinos = [
+    {
+      nome: "Paris",
+      info: "Teste",
+      image: "https://www.melhoresdestinos.com.br/wp-content/uploads/2019/02/passagens-aereas-paris-capa2019-02.jpg"
+    },
+
+    {
+      nome: "Zurique",
+      info: "Teste",
+      image: "https://www.qualviagem.com.br/wp-content/uploads/2016/09/iStock_86150945_SMALL.jpg"
+    },
+
+  ] 
+
+
   return (
-    <div className="background">
-      <Navbar/>
-      <Card/>
-    </div>
+    <>
+      <div className="background">
+        <Navbar/>
+        <CardDestaque/>
+      </div>
+      <Title>Destinos</Title>
+      <section className="flex flex-wrap" style={{marginLeft: '18rem', marginTop: '1rem'}}>
+        {destinos.map((destino)=>{
+          return <CardMenor dados={destino}/>
+        })}
+
+        
+      </section>
+
+      <Title>Hotéis</Title>
+    </>
   );
 }
---
+++ src/components/layout/Title.jsx
@@ -0,0 +1,8 @@
+
+export default function Title({children}){
+    return(
+        <h2 className="text-xl font-semibold" style={{marginLeft: '15rem', marginTop: '1rem'}}>
+            {children}
+        </h2>
+    )
+}
